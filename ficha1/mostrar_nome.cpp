5032233c9394050d2b397c8909dff4dd6816dbcf
Leonardo <lcat140507@gmail.com>
2026-03-03 21:11:14 +0000

    criar programa mostrar nome

---
+++ ficha1/mostrar_nome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    cout << "Digite seu nome: ";
+
+    string nome;
+    cin >> nome;
+
+    cout << "Ola, " << nome << "!" << endl;
+
+    return 0;
+    system("pause");
+}
