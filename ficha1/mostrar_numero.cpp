e193ba2d43bbbb1a24936e358bc62a7550a006b6
Leonardo <lcat140507@gmail.com>
2026-03-03 20:51:50 +0000

    criar programa de número de aluno

---
+++ ficha1/mostrar_numero.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Digite o seu número de aluno: ";
+
+    int numero;
+    cin >> numero;
+    cout << "O número digitado foi: " << numero << endl;
+
+    return 0;
+
+}
