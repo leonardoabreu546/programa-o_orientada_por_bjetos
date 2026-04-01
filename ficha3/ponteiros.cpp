b7bda129bf981525c342b2d9eefe63f7e1647a41
Leonardo <lcat140507@gmail.com>
2026-04-01 23:19:05 +0100

    criar exemplo ponteiros

---
+++ ficha3/ponteiros.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v[7] = {1, 2, 3, 4, 5, 6, 7};
+    int *p = v;
+
+    cout << *p++ << " ";
+
+    cout << *(p + 2) << " ";
+
+    cout << *(++p) << " ";
+
+    cout << *(++p + 3) << " ";
+
+    cout << *(++p - 4) << endl;
+
+    p = v;
+    for (int i = 0; i < 7; i++) {
+        cout << *p++;
+        if (i < 6) cout << ",";
+        else cout << ".";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+1 4 
